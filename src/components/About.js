ec0d0ef1c6c97f989fc4d5eed99f2c5d97e11ca7
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-07-08 19:05:52 +0530

    added front end components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+// import React, { useContext, useEffect } from 'react';
+
+const About = () => {
+
+  return (
+    <div>
+      this is About
+    </div>
+  )
+}
+
+export default About
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+    return (
+        <NoteContext.Provider value={{}}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
