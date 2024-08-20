fac022156974a184f54b0758f70aed86cd2d5bf9
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-08-21 04:25:38 +0530

    configured notestate

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "https://inotebook-backend-ytnk.onrender.com"
+  const host = "http://localhost:5000"
   const notesInitial = []
 
   const [notes, setNotes] = useState(notesInitial)
